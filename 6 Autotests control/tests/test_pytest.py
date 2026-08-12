3b3afc46af3603b76c66988e048b70c6a57e5f7c
denis.sazonov <denis.sazonov@mytona.com>
2026-08-12 09:36:31 +0200

    NEXT: folder 6.1

---
+++ 6 Autotests control/tests/test_pytest.py
@@ -0,0 +1,13 @@
+def test_user_login():
+    print("Hello!")
+
+
+class TestUserLogin:
+    def __int__(self):
+        pass
+
+    def test_1(self):
+        pass
+
+    def test_2(self):
+        pass
