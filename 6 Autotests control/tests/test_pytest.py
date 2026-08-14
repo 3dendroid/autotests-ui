90022cab0260f20ddb147b524906af8e1d4902f6
denis.sazonov <denis.sazonov@mytona.com>
2026-08-14 09:11:47 +0200

    NEXT: folder 6.1

--- 6 Autotests control/tests/test_pytest.py
+++ 6 Autotests control/tests/test_pytest.py
@@ -11,3 +11,11 @@ class TestUserLogin:
 
     def test_2(self):
         pass
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, "(2 + 2) !=5"
