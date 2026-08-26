c78a7df0aa4f1b2b29fd3b84466b86586e49d359
denis.sazonov <denis.sazonov@mytona.com>
2026-08-26 18:55:30 +0200

    NEXT: continue folder 6.5, test_fixture.py

--- 6 Autotests control/6.5 fixtures/test_fixture.py
+++ 6 Autotests control/6.5 fixtures/test_fixture.py
@@ -1,1 +1,34 @@
-# NEXT
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def send_analytics():
+    print("[AUTOUSE] Sending data to analytic server")
+
+
+@pytest.fixture(scope="session")
+def settings():
+    print("[SESSION] Initialization session settings")
+
+
+@pytest.fixture(scope="class")
+def user():
+    print("[CLASS] Creating class one time for classes")
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("[FUNCTION] Open browser everytime")
+
+
+class TestUserFlow:
+    def test_user_login(self, settings, user, browser):
+        pass
+
+    def test_user_can_create_course(self, settings, user, browser):
+        pass
+
+
+class TestAccountFlow:
+    def test_user_account(self, settings, user, browser):
+        pass
